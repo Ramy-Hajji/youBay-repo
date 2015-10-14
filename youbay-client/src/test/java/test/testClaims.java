c5291d8e10d8e61693d1a47750fdcdb37f5b6e28
Cyrine <Cyrine@Cyrine-PC>
2015-10-14 11:49:39 +0100

    Signed-off-by: Cyrine <Cyrine@Cyrine-PC>

--- youbay-client/src/test/java/test/testClaims.java
+++ youbay-client/src/test/java/test/testClaims.java
@@ -5,21 +5,21 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import tn.youbay.entities.Claims;
-import tn.youbay.services.interfaces.IAccountsService;
 import tn.youbay.services.interfaces.IClaimService;
 
 public class testClaims {
-public static void main(String[] args) {
-	try {
-		Context context =  new InitialContext();
-		IClaimService remote;
-		remote=(IClaimService) context.lookup("/youbay-ejb/ClaimService!tn.youbay.services.IClaimService");
-		remote.AddClaiim(new Claims("Reee", 8));
-		remote.DeleteClaiim(new Claims(5,"Reee", 8));
+	public static void main(String[] args) {
+		try {
+			Context context = new InitialContext();
+			IClaimService remote;
+			remote = (IClaimService) context
+					.lookup("/youbay-ejb/ClaimService!tn.youbay.services.IClaimService");
+			remote.AddClaiim(new Claims("Reee", 8));
+			remote.DeleteClaiim(new Claims(5, "Reee", 8));
 		} catch (NamingException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-		
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		}
 	}
-}
 }
